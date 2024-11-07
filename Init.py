1ea2b8ef07019514321f8a51bdd4b01864b23b2b
pascal369 <138413758+pascal369@users.noreply.github.com>
2024-11-07 21:20:12 +0900

    Add files via upload

---
+++ Init.py
@@ -0,0 +1,9 @@
+#***************************************************************************
+#*    Copyright (C) 2020 CADENAS GmbH
+#*
+#*    This library is free software
+#***************************************************************************
+import os
+from os.path import dirname
+from os.path import realpath
+module_path = dirname(realpath(__file__))
---
+++ InitGui.py
@@ -0,0 +1,64 @@
+#***************************************************************************
+#*    Copyright (C) 2023 
+#*    This library is free software
+#***************************************************************************
+
+import os
+import sys
+import FreeCAD
+import FreeCADGui
+
+class FcdFittingsShowCommand:
+    def GetResources(self):
+        from init import module_path
+        print(module_path)
+        return { 
+          'Pixmap': os.path.join(module_path, "icons", "FcdFittings.svg"),
+          'MenuText': "FcdFittings",
+          'ToolTip': "Show/Hide FcdFittings"}
+
+    def IsActive(self):
+        import Ductile
+        Ductile
+        return True
+
+    def Activated(self):
+        try:
+          import Ductile
+          Ductile.main.d.show()
+        except Exception as e:
+          FreeCAD.Console.PrintError(str(e) + "\n")
+
+    def IsActive(self):
+        import Ductile
+        return not FreeCAD.ActiveDocument is None
+
+class FcdFittingsWB(FreeCADGui.Workbench):
+    def __init__(self):
+        from Init import module_path
+        print(module_path)
+        self.__class__.Icon = os.path.join(module_path, "icons", "FcdFittings.svg")
+        self.__class__.MenuText = "FcdFittings"
+        self.__class__.ToolTip = "FcdFittings by Pascal"
+
+    def Initialize(self):
+        self.commandList = ["FcdFittings_Show"]
+        self.appendToolbar("&FcdFittings", self.commandList)
+        self.appendMenu("&FcdFittings", self.commandList)
+
+    def Activated(self):
+        import Ductile
+        Ductile.main.d.show()
+        return
+
+    def Deactivated(self):
+        return
+
+    def ContextMenu(self, recipient):
+        return
+
+    def GetClassName(self): 
+        return "Gui::PythonWorkbench"
+    
+FreeCADGui.addWorkbench(FcdFittingsWB())
+FreeCADGui.addCommand("FcdFittings_Show", FcdFittingsShowCommand())    
